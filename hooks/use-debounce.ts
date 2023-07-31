8692593ce1936e926aedf4a915e646e0c4ad1800
izanamiah <jeremiahliu@live.com>
2023-07-31 11:52:15 -0400

    added search bar with debounce hook

---
+++ hooks/use-debounce.ts
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+
+//used to prevent high frequency database query on every key stroke
+export function useDebounce<T>(value: T, delay?: number): T {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+  useEffect(() => {
+    const timer = setTimeout(() => setDebouncedValue(value), delay || 500);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
